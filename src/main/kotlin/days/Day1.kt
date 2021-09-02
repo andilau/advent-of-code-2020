820adbdcdfc896563ab39d9671885dbd198ba8dd
Andi Lau <andi@herrlau.de>
2021-09-02 13:09:43 +0200

    Day1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,38 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return inputList
+            .map { it.toInt() }
+            .findPairOfSum(2020)
+            ?.let { (a, b) -> a * b }
+            ?: 0
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return inputList
+            .map { it.toInt() }
+            .findTripleOfSum(2020)
+            ?.let { (a, b, c) -> a * b * c }
+            ?: 0
     }
+}
+
+fun List<Int>.findPairOfSum(sum: Int): Pair<Int, Int>? {
+    for (first in this)
+        for (second in this)
+            if (first + second == sum)
+                return first to second
+    return null
+}
+
+fun List<Int>.findTripleOfSum(sum: Int): Triple<Int, Int, Int>? {
+    forEach { first ->
+        forEach { second ->
+            forEach { third ->
+                if (first + second + third == sum)
+                    return Triple(first, second, third)
+            }
+        }
+    }
+    return null
 }
