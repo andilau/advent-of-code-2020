e9e3162074159ceee0da1cfd38d14acc4b466670
Andi Lau <andi@herrlau.de>
2021-09-07 17:25:26 +0200

    Day10 (Adapter Array)

---
+++ src/main/kotlin/days/Day10.kt
@@ -0,0 +1,34 @@
+package days
+
+class Day10 : Day(10) {
+    override fun partOne(): Int {
+        return inputList
+            .asSequence()
+            .map { it.toInt() }
+            .plus(0)
+            .sorted()
+            .zipWithNext { a, b -> b - a }
+            .groupingBy { it }
+            .eachCount()
+            .run { getOrDefault(1, 1) * (1 + getOrDefault(3, 1)) }
+    }
+
+    override fun partTwo(): Long {
+        val input: List<Int> = inputList.map { it.toInt() }
+        val adapters = input
+            .plus(0)
+            .plus(input.maxOrNull()!! + 3)
+            .sorted()
+
+        val combinationsByAdapter: MutableMap<Int, Long> = mutableMapOf(0 to 1L)
+
+        for (adapter in adapters.drop(1)) {
+            combinationsByAdapter[adapter] =
+                combinationsByAdapter.getOrDefault(adapter - 1, 0) +
+                        combinationsByAdapter.getOrDefault(adapter - 2, 0) +
+                        combinationsByAdapter.getOrDefault(adapter - 3, 0)
+        }
+        return combinationsByAdapter.getValue(adapters.last())
+    }
+
+}
