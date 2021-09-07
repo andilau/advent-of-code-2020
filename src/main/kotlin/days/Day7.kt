4ef189b5882fcb68631f18584255396b677e9c57
Andi Lau <andi@herrlau.de>
2021-09-07 17:27:04 +0200

    Day7 (Handy Haversacks)

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -1,17 +1,5 @@
 package days
 
-/*
-light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.
-*/
-
 class Day7 : Day(7) {
     private val bagRules: Set<BagRule> = parseLines(inputList)
 
@@ -34,21 +22,21 @@ class Day7 : Day(7) {
             .filter { it.parent == bag }
             .sumOf { it.amount * calculateBags(it.child) } + 1
     }
-}
 
-data class BagRule(val parent: String, val amount: Int, val child: String)
+    data class BagRule(val parent: String, val amount: Int, val child: String)
 
-fun parseLines(lines: List<String>): Set<BagRule> {
-    return lines.flatMap { line ->
-        val parts = line.split(" ")
-        val parent = parts.take(2).joinToString(" ")
-        parts.drop(4).windowed(4, 4, false).map { child ->
-            BagRule(
-                parent,
-                child.first().toInt(),
-                child.drop(1).take(2).joinToString(" ")
-            )
-        }
-    }.toSet()
+    fun parseLines(lines: List<String>): Set<BagRule> {
+        return lines.flatMap { line ->
+            val parts = line.split(" ")
+            val parent = parts.take(2).joinToString(" ")
+            parts.drop(4).windowed(4, 4, false).map { child ->
+                BagRule(
+                    parent,
+                    child.first().toInt(),
+                    child.drop(1).take(2).joinToString(" ")
+                )
+            }
+        }.toSet()
 
+    }
 }
