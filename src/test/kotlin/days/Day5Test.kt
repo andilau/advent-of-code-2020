a1c216662b2345918cbcf7551c6d69141ae11895
Andi Lau <andi@herrlau.de>
2021-09-07 17:27:26 +0200

    Day5 (Binary Boarding)

--- src/test/kotlin/days/Day5Test.kt
+++ src/test/kotlin/days/Day5Test.kt
@@ -2,26 +2,14 @@ package days
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
-import org.junit.Ignore
 import org.junit.Test
-import java.math.BigInteger
 
 class Day5Test {
     private val day = Day5()
-
-//    BFFFBBFRRR: row 70, column 7, seat ID 567.
-//    FFFBBBFRRR: row 14, column 7, seat ID 119.
-//    BBFFBBFRLL: row 102, column 4, seat ID 820.
 
     @Test
     fun testPartOne() {
         BoardingPass.parse("BFFFBBFRRR").getSeatId()
         assertThat(day.partOne(), `is`(820))
-    }
-
-    @Test
-    @Ignore
-    fun testPartTwo() {
-        assertThat(day.partTwo(), `is`(0))
     }
 }
