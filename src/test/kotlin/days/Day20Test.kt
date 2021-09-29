16049665d7ef5d5de5dab70fda4c58edc6a612ae
Andi Lau <andi@herrlau.de>
2021-09-29 10:52:16 +0200

    Day20 (Jurassic Jigsaw) small improvements

--- src/test/kotlin/days/Day20Test.kt
+++ src/test/kotlin/days/Day20Test.kt
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+@DisplayName("Day 20")
 class Day20Test {
     private val day = Day20()
 
+    @DisplayName("Part 1")
     @Test
     fun testPartOne() {
         assertThat(day.partOne()).isEqualTo(20_899_048_083_289)
     }
 
     @Test
+    @DisplayName("Part 2")
     fun testPartTwo() {
         assertThat(day.partTwo()).isEqualTo(273)
     }
@@ -22,18 +25,17 @@ class Day20Test {
     @DisplayName("Tile Test")
     @Nested
     inner class TileTest {
-        private val tileRotated = Tile(0, "gda, heb, ifc".split(", "))
-        private val tileFlipped = Tile(0, "cba, fed, ihg".split(", "))
+        private val original = Tile(0, "abc\ndef\nghi".lines())
+        private val tileRotated = Tile(0, "gda\nheb\nifc".lines())
+        private val tileFlipped = Tile(0, "cba\nfed\nihg".lines())
 
         @Test
         fun shouldRotate() {
-            val tile = Tile(0, "abc\ndef\nghi".lines())
-            assertThat(tile.rotate()).isEqualTo(tileRotated)
+            assertThat(original.rotate()).isEqualTo(tileRotated)
         }
 
         @Test
         fun shouldRotateToSelf() {
-            val original = Tile(0, "abc\ndef\nghi".lines())
             var tile = original.copy()
             repeat(4) { tile = tile.rotate() }
             assertThat(tile).isEqualTo(original)
@@ -41,13 +43,11 @@ class Day20Test {
 
         @Test
         fun shouldFlip() {
-            val tile = Tile(0, "abc\ndef\nghi".lines())
-            assertThat(tile.flip()).isEqualTo(tileFlipped)
+            assertThat(original.flip()).isEqualTo(tileFlipped)
         }
 
         @Test
         fun shouldFlipToSelf() {
-            val original = Tile(0, "abc\ndef\nghi".lines())
             var tile = original.copy()
             repeat(2) { tile = tile.flip() }
             assertThat(tile).isEqualTo(original)
@@ -56,7 +56,6 @@ class Day20Test {
         @Test
         fun variations() {
             val tile = Tile(0, "abc\ndef\nghi".lines())
-            tile.allVariations().forEach { println(it) }
             assertThat(tile.allVariations().toList())
                 .isNotEmpty
                 .hasSize(8)
