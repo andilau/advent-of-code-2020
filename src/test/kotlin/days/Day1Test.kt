6e9d658ab07871b18570bcd72e94bfa330337582
Andi Lau <andi@herrlau.de>
2021-10-13 16:37:20 +0200

    Mod Day1Test enable test, remove actual

--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -1,7 +1,6 @@
 package days
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -9,36 +8,19 @@ import org.junit.jupiter.api.Test
 @DisplayName("Day 1")
 class Day1Test {
     private val dayOne = Day1()
-    private val input = listOf(1721, 979, 366, 299, 675, 1456)
 
+    @Test
     @DisplayName("Part 1")
-    @Nested
-    inner class Part1 {
-        @Test
-        @Disabled
-        fun Example() {
-            assertThat(dayOne.partOne()).isEqualTo(514_579)
-        }
-
-        @Test
-        fun Actual() {
-            assertThat(dayOne.partOne()).isEqualTo(1_007_331)
-        }
+    fun testPart1() {
+        assertThat(dayOne.partOne()).isEqualTo(514_579)
     }
 
     @DisplayName("Part 2")
     @Nested
     inner class Part2 {
         @Test
-        @Disabled
-        fun Example() {
-            assertThat(dayOne.partTwo()).isEqualTo(241861950)
+        fun testPart2() {
+            assertThat(dayOne.partTwo()).isEqualTo(241_861_950)
         }
-
-        @Test
-        fun Actual() {
-            assertThat(dayOne.partTwo()).isEqualTo(48914340)
-        }
-
     }
 }
--- src/test/kotlin/days/Day21Test.kt
+++ src/test/kotlin/days/Day21Test.kt
@@ -9,11 +9,13 @@ class Day21Test {
     private val day = Day21()
 
     @Test
+    @DisplayName("Part 1")
     fun testPartOne() {
         assertThat(day.partOne()).isEqualTo(5)
     }
 
     @Test
+    @DisplayName("Part 2")
     fun testPartTwo() {
         assertThat(day.partTwo()).isEqualTo("mxmxvkd,sqjhc,fvjkl")
     }
