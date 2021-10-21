62b5088ce472932ed3b8bd9a80306e464f7432a0
Andi Lau <andi@herrlau.de>
2021-10-21 13:00:43 +0200

    Mod Day1Test remove inner class

--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -2,7 +2,6 @@ package days
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @DisplayName("Day 1")
@@ -15,12 +14,9 @@ class Day1Test {
         assertThat(dayOne.partOne()).isEqualTo(514_579)
     }
 
+    @Test
     @DisplayName("Part 2")
-    @Nested
-    inner class Part2 {
-        @Test
-        fun testPart2() {
-            assertThat(dayOne.partTwo()).isEqualTo(241_861_950)
-        }
+    fun testPart2() {
+        assertThat(dayOne.partTwo()).isEqualTo(241_861_950)
     }
 }
